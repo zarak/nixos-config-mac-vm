0bc070c25709446561c92a293ea89e3fc3efe3db
Zarak <7254237+zarak@users.noreply.github.com>
2024-03-25 16:03:16 +0500

    Add type1 theme

--- programs/rofi/default.nix
+++ programs/rofi/default.nix
@@ -4,7 +4,7 @@
   programs.rofi = {
     enable = true;
     terminal = "${pkgs.wezterm}/bin/wezterm";
-    theme = ./spotlight_theme.rasi;
+    theme = ./type1_theme.rasi;
     plugins = [ pkgs.rofi-file-browser ];
     extraConfig = {
       modi = "window,run,ssh,file-browser,file-browser-extended";
