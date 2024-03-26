afeded8ce51ed10f0f192ff4cd528f4eb94c28a1
Zarak <7254237+zarak@users.noreply.github.com>
2024-03-26 11:31:41 +0500

    Update options to nixos-unstable

--- configuration.nix
+++ configuration.nix
@@ -84,8 +84,8 @@
   services.xserver.autoRepeatInterval = 25;
 
   # Configure keymap in X11
-  services.xserver.layout = "us";
-  services.xserver.xkbOptions = "eurosign:e,caps:escape";
+  services.xserver.xkb.layout = "us";
+  services.xserver.xkb.options = "eurosign:e,caps:escape";
 
   # Enable CUPS to print documents.
   # services.printing.enable = true;
