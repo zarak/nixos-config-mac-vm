f373ff20744ac62b42f05eb623e0e790daf56034
zarak <7254237+zarak@users.noreply.github.com>
2024-03-28 01:30:11 +0500

    Add program for vmware clipboard

--- configuration.nix
+++ configuration.nix
@@ -115,6 +115,7 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     git
+    gtkmm3
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     neovim
     wezterm
