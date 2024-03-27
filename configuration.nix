a403de63ca03e61759025f1fd19e99b771edb8ce
zarak <7254237+zarak@users.noreply.github.com>
2024-03-28 01:34:48 +0500

    Fill wallpaper

--- configuration.nix
+++ configuration.nix
@@ -67,7 +67,7 @@
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
-
+  services.xserver.desktopManager.wallpaper.mode = "fill";
 
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.lightdm.enable = true;
