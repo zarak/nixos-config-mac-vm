9a2dc65ed2355ed2f6c2e4d8bb35338ceca4dd9e
zarak <7254237+zarak@users.noreply.github.com>
2024-03-31 10:30:54 +0500

    Initial flake

---
+++ scripts/setup-haskell-project/flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Setup a haskell project with cabal and create a repository on
+  GitHub";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: 
+      let
+        pkgs = import nixpkgs { inherit system; };
+        scriptName = "setup-haskell-project";
+        script = 
+          (pkgs.writeScriptBin 
+            scriptName 
+            (builtins.readFile ./setup-haskell-project.sh)).overrideAttrs(old: {
+              buildCommand = "{old.buildCommand}\n patchShebangs $out";
+        });
+
+
+      in rec {
+        defaultPackage = packages.script;
+        packages.script = pkgs.symlinkJoin {
+          name = scriptName;
+          paths = [ script pkgs.cabal ];
+          buildInputs = [ pkgs.makeWrapper ];
+          postBuild = "wrapProgram $out/bin/${scriptName} --prefix PATH : $out/bin";
+        };
+      }
+    );
+}
