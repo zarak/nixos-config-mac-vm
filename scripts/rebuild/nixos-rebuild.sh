229dd588c7ec83b183706615ba21e816761a50cf
zarak <7254237+zarak@users.noreply.github.com>
2024-04-02 08:22:58 +0500

    58 current  2024-04-01 10:17:31  24.05.20240325.57e6b3a   6.6.22                          *
    58 current  2024-04-01 10:17:31  24.05.20240325.57e6b3a   6.6.22                          *
    
    Rewrite script to exit on error
    
    Remove closure
    
    58 current  2024-04-01 10:17:31  24.05.20240325.57e6b3a   6.6.22                          *
    
    Trigger if block only if nixos-rebuild fails
    
    58 current  2024-04-01 10:17:31  24.05.20240325.57e6b3a   6.6.22                          *
    
    Wait, an if statement runs in a subshell?
    
    Remove closure
    
    58 current  2024-04-01 10:17:31  24.05.20240325.57e6b3a   6.6.22                          *
    
    echo diagnostics
    
    Remove closure
    
    58 current  2024-04-01 10:17:31  24.05.20240325.57e6b3a   6.6.22                          *
    
    tee is producing 0 error code
    
    Remove nix-closure
    
    58 current  2024-04-01 10:17:31  24.05.20240325.57e6b3a   6.6.22                          *

--- scripts/rebuild/nixos-rebuild.sh
+++ scripts/rebuild/nixos-rebuild.sh
@@ -27,7 +27,7 @@ git diff -U0 '*.nix'
 echo "NixOS Rebuilding..."
 
 # Rebuild, output simplified errors, log trackebacks
-sudo nixos-rebuild switch &>nixos-switch.log || (cat nixos-switch.log | grep --color error && exit 1)
+sudo nixos-rebuild switch &>nixos-switch.log || (grep --color error < nixos-switch.log && exit 1)
 
 # Get current generation metadata
 current=$(nixos-rebuild list-generations | grep current)
