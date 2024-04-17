861ac7f0ed6c9f42b4ae17e3f888f27f39bed2de
zarak <7254237+zarak@users.noreply.github.com>
2024-04-17 23:21:27 +0500

    Add emmet language server

--- programs/neovim/init.lua
+++ programs/neovim/init.lua
@@ -496,6 +496,8 @@ require 'lspconfig'.bashls.setup { on_attach = on_attach }
 
 require 'lspconfig'.phpactor.setup { on_attach = on_attach }
 
+require 'lspconfig'.emmet_ls.setup { on_attach = on_attach, filetypes = { 'html', 'css', 'php' } }
+
 require 'lspconfig'.lua_ls.setup {
   on_init = function(client)
     local path = client.workspace_folders[1].name
