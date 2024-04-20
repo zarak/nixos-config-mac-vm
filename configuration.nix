d99139ff6b747b70c306b7d6e17ebc2943dd72e2
zarak <7254237+zarak@users.noreply.github.com>
2024-04-20 17:40:20 +0500

    Put in system config

--- configuration.nix
+++ configuration.nix
@@ -195,6 +195,21 @@
     '';
   };
 
+  # NixOS has built-in modules to enable 1Password
+  # along with some pre-packaged configuration to make
+  # it work nicely. You can search what options exist
+  # in NixOS at https://search.nixos.org/options
+
+  # Enables the 1Password CLI
+  programs._1password = { enable = true; };
+
+  # Enables the 1Password desktop app
+  programs._1password-gui = {
+  enable = true;
+  # this makes system auth etc. work properly
+  polkitPolicyOwners = [ "zarak" ];
+  };
+
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
--- home.nix
+++ home.nix
@@ -275,21 +275,6 @@ in {
     x11.enable = true;
   };
 
-  # NixOS has built-in modules to enable 1Password
-  # along with some pre-packaged configuration to make
-  # it work nicely. You can search what options exist
-  # in NixOS at https://search.nixos.org/options
-
-  # Enables the 1Password CLI
-  programs._1password = { enable = true; };
-
-  # Enables the 1Password desktop app
-  programs._1password-gui = {
-  enable = true;
-  # this makes system auth etc. work properly
-  polkitPolicyOwners = [ "<your-linux-username>" ];
-  };
-
   # xdg.configFile = {
   # "nvim/coc-settings.json".text = cocSettings;
   # };
